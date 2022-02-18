3c30a7933f81cc8ccc476a4d94ad62d2354ecfc7
Katelyn Lydeen <klydeen@hawk.iit.edu>
2022-02-18 13:54:58 -0600

    Initial Commit

---
+++ Shared/_D_Schrodinger_EquationApp.swift
@@ -0,0 +1,17 @@
+//
+//  _D_Schrodinger_EquationApp.swift
+//  Shared
+//
+//  Created by Katelyn Lydeen on 2/18/22.
+//
+
+import SwiftUI
+
+@main
+struct _D_Schrodinger_EquationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
